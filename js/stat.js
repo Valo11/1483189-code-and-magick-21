8375ff72909046aa7ccdfec6d75665662f93b59d
Valo11 <vladimir92.stepanyan@gmail.com>
2020-09-24 11:39:18 +0300

    создал ветку module2-task1 и  создал файл js/stat.js и подключил его почти выполнил задание холст внес правки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,20 +1,20 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 40;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 var MAX_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -25,48 +25,28 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(
-    ctx,
-    CLOUD_X + GAP,
-    CLOUD_Y + GAP,
-    'rgba(0, 0, 0, 0.7)'
-  );
-  renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    '#fff'
-  );
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 130, 25);
   ctx.fillText('Список результатов:', 130, 45);
-  console.log(players);
-  console.log(times);
 
   var maxTime = getMaxElement(times);
-  var  onePoint = MAX_HEIGHT / maxTime;
+  var onePoint = MAX_HEIGHT / maxTime;
 
   for (var i = 0; i < players.length; i++) {
-    if (players[i] == 'Вы') {
+    if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(44,' + (130 + i * 30) + ', 201, 1)';
     }
-    ctx.fillRect (140 + i * 100, 250, BAR_HEIGHT, -times[i] * onePoint);
+    ctx.fillRect(140 + i * 100, 250, BAR_HEIGHT, -times[i] * onePoint);
 
     ctx.fillStyle = '#000';
-    ctx.fillText(
-      players[i],
-      CLOUD_X + (GAP * 4) + (FONT_GAP * 4 + BAR_HEIGHT) * i,
-      CLOUD_Y + (GAP * 25)
-    );
-    ctx.fillText(
-      Math.round(times[i]),
-      140 + i * 100,
-      230 - times[i] * onePoint
-    )
+    ctx.fillText(players[i], CLOUD_X + (GAP * 4) + (FONT_GAP * 4 + BAR_HEIGHT) * i, CLOUD_Y + (GAP * 25));
+    ctx.fillText(Math.round(times[i]), 140 + i * 100, 230 - times[i] * onePoint);
   }
 };
